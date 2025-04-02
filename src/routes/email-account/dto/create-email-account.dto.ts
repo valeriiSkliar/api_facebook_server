fcc1856eb0f770dcd6d77ab6b7cb4c9a4a2fcbd3
vs <valeriisklyarov@gmail.com>
2025-04-02 21:17:41 +0300

    feat(create-email-account-dto): add transformation for connection_details

--- src/routes/email-account/dto/create-email-account.dto.ts
+++ src/routes/email-account/dto/create-email-account.dto.ts
@@ -1,4 +1,11 @@
-import { IsString, IsEmail, IsOptional, IsBoolean, IsJSON } from 'class-validator';
+import {
+  IsString,
+  IsEmail,
+  IsOptional,
+  IsBoolean,
+  IsJSON,
+} from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class CreateEmailAccountDto {
   @IsEmail()
@@ -9,6 +16,12 @@ export class CreateEmailAccountDto {
 
   @IsOptional()
   @IsJSON()
+  @Transform(({ value }) => {
+    if (typeof value === 'object') {
+      return JSON.stringify(value);
+    }
+    return value as string;
+  })
   connection_details?: Record<string, any>;
 
   @IsString()

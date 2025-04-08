543dfa12673a91f3e87fd53c522a59810596bb41
vs <valeriisklyarov@gmail.com>
2025-04-08 10:18:25 +0300

    refactor(scraper): improve error handling and response validation

---
+++ src/scrapers/common/interfaces/i-scraper.ts
@@ -0,0 +1,31 @@
+import { RequestMetadata } from '@src/api/requests/request-manager-service';
+import { ScraperResult } from '@src/scrapers/facebook/models/facebook-scraper-result';
+
+// /**
+//  * Определяет структуру результата работы скрапера.
+//  */
+// export interface ScraperResult {
+//   /** Флаг успешности выполнения операции скрапинга. */
+//   success: boolean;
+//   /** Собранные данные. Тип может быть уточнен. */
+//   data?: any; // TODO: Уточнить тип данных или использовать union type
+//   /** Информация об ошибке, если операция не удалась. */
+//   error?: string | object;
+//   /** Путь к файлу, в который могли быть сохранены результаты (например, для больших объемов данных). */
+//   outputPath?: string;
+//   /** Количество успешно собранных элементов. */
+//   count?: number;
+// }
+
+/**
+ * Интерфейс для реализации скрапера.
+ */
+
+export interface IScraper {
+  /**
+   * Основной метод для запуска процесса скрапинга.
+   * @param request - Метаданные запроса, содержащие параметры и тип.
+   * @returns Promise, разрешающийся результатом скрапинга.
+   */
+  scrape(request: RequestMetadata): Promise<ScraperResult>;
+}

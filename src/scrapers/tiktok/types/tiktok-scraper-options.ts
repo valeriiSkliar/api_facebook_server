05ffd6b7d7b5d6da2f1aa73a740e9cc1b8c80dbb
vs <valeriisklyarov@gmail.com>
2025-04-08 23:23:27 +0300

    delete(types): remove unused TikTok scraper context and options

--- src/scrapers/tiktok/types/tiktok-scraper-options.ts
+++
@@ -1,3 +0,0 @@
-import { ScraperOptions } from '@src/scrapers/common/interfaces/scraper-options.interface';
-import { TiktokLibraryQuery } from '../models/tiktok-library-query';
-export type TikTokScraperOptions = ScraperOptions<TiktokLibraryQuery>;
